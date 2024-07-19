36876dd2b8e7b95a0ca785664eaad4e7555049b4
Sergio Zaldivar <zayser99@hotmail.com>
2024-07-19 11:59:19 -0600

    first commit

---
+++ src/GifExpertApp.jsx
@@ -0,0 +1,31 @@
+import { useState } from "react";
+import { AddCategory, GifGrid } from "./components";
+
+export const GifExpertApp = () => {
+    const [categories, setCategories] = useState(['code']);
+
+    const onAddCategory = (newCategory) => {
+        if (categories.includes(newCategory)) return;
+        setCategories([newCategory, ...categories]);
+        // setCategories(cat=>[...cat, newCategory]);
+    }
+
+
+    return (
+        <>
+
+            <h1>GiftExpertApp</h1>
+
+            <AddCategory
+                onNewCategory={value => onAddCategory(value)}
+            />
+
+
+            {
+                categories.map((category, i) => (
+                    <GifGrid key={category} category={category} />
+                ))
+            }
+        </>
+    )
+}
