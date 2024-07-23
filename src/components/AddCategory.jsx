a0e98a20ced6e9865f905ec172ebee936b38cde0
Sergio Zaldivar <zayser99@hotmail.com>
2024-07-23 12:08:33 -0600

    simular sumbmit del formulario

--- src/components/AddCategory.jsx
+++ src/components/AddCategory.jsx
@@ -17,7 +17,7 @@ export const AddCategory = ({ onNewCategory }) => {
     }
 
     return (
-        <form onSubmit={onSubmit}>
+        <form onSubmit={onSubmit} aria-label='form'>
             <input
                 type="text"
                 placeholder="Buscar Gifs"
--- test/components/AddCategory.test.jsx
+++ test/components/AddCategory.test.jsx
@@ -3,17 +3,34 @@ import { AddCategory } from "../../src/components/AddCategory"
 
 
 describe('pruebas en <AddCategory/>', () => {
-    test('debe de cambiar el valor de la caja de texto ', () => {
 
+
+    test('debe de cambiar el valor de la caja de texto ', () => {
         render(<AddCategory onNewCategory={() => { }} />);
         const input = screen.getByRole('textbox');
 
         fireEvent.input(input, { target: { value: 'Saitama' } });
 
         expect(input.value).toBe('Saitama');
-
         // screen.debug();
+    });
 
-    })
+
+    test('debe de llamar onNewCategory si el input tiene un valor', () => {
+        const inputValue = 'Saitama';
+        // TODO: ????
+        render(<AddCategory onNewCategory={() => { }} />);
+
+        const input = screen.getByRole('textbox');
+        const form = screen.getByRole('form');
+
+        fireEvent.input(input, { target: { value: inputValue } });
+        fireEvent.submit(form );
+
+        expect(input.value).toBe('');
+    });
+
+
+
 });
 
