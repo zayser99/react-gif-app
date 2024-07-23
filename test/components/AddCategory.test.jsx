6416478f411b5f656e38ab849189d52a547cba6d
Sergio Zaldivar <zayser99@hotmail.com>
2024-07-23 12:48:40 -0600

    Jest Functions

--- test/components/AddCategory.test.jsx
+++ test/components/AddCategory.test.jsx
@@ -18,16 +18,32 @@ describe('pruebas en <AddCategory/>', () => {
 
     test('debe de llamar onNewCategory si el input tiene un valor', () => {
         const inputValue = 'Saitama';
-        // TODO: ????
-        render(<AddCategory onNewCategory={() => { }} />);
+        const onNewCategory = jest.fn(); // sirve para simular una funcion 
+
+        render(<AddCategory onNewCategory={onNewCategory} />);
 
         const input = screen.getByRole('textbox');
         const form = screen.getByRole('form');
 
         fireEvent.input(input, { target: { value: inputValue } });
-        fireEvent.submit(form );
+        fireEvent.submit(form);
 
         expect(input.value).toBe('');
+        expect(onNewCategory).toHaveBeenCalled();  //comprobamos que nuestra funcion sumulada se ejecute
+        expect(onNewCategory).toHaveBeenCalledTimes(1); //comprobamos que nuestra funcion simulada se ejecute N veces
+        expect(onNewCategory).toHaveBeenCalledWith(inputValue);// evaula que mi funcion simulada se ejecute con el valor que yo esperaba 
+    });
+
+    test('no debe llamar el onNewCategory  si el input esta vacio', () => {
+        const onNewCategory = jest.fn(); // sirve para simular una funcion 
+        render(<AddCategory onNewCategory={onNewCategory} />);
+
+        const form = screen.getByRole('form');
+        fireEvent.submit(form);
+
+        expect(onNewCategory).toHaveBeenCalledTimes(0);
+        expect(onNewCategory).not.toHaveBeenCalled();
+
     });
 
 
